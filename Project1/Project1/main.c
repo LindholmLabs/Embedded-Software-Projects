733b8485871e58a0c242c6fd836f3d994b679bbf
William Lindholm <william_lindholm@outlook.com>
2024-10-09 11:22:23 +0100

    Project 1 created

---
+++ Project1/Project1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project1.c
+ *
+ * Created: 2024-10-09 11:21:00
+ * Author : William
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
